6a8bc1457607a5a097197ee7b3883e729c71cac4
Chelloju Ramu <chelloju@outlook.com>
2021-02-21 23:52:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ orange = [40, 100, 60];
 green = [75, 100, 40];
 blue = [196, 77, 55];
 purple = [280, 50, 60];
+letterColors = [red, orange, green, blue, purple];
 
 message = 'Ramu Chelloju!';
 
-drawName(message, green);
+drawName(message, letterColors);
 bounceBubbles();
